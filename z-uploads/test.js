474a890981f0282f304ecf9bfbea9262803c8a7d
Harsh Rawat <harshrawat130@gmail.com>
2023-09-29 21:06:31 +0530

    Yay completed

---
+++ z-uploads/test.js
@@ -0,0 +1,16 @@
+const aha=new Array("nnn","Agriculture, Forestry and Fishing","nisharg","nisha");
+
+// { $regex: '.*' + value + '.*' }
+const hh='Agri';
+console.log(aha[1].indexOf('Agriy'));
+
+
+const sg=new Set();
+sg.add(1);
+sg.add(1);
+sg.add(12);
+sg.add(1);
+sg.add(15);
+
+const fdf=
+console.log([...sg]);
